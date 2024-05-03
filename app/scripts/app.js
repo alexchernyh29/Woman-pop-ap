c12741b7e397f343692c3a4f7b4a57076237fcaa
Alexander Chernyh <alexchernyh2000@mail.ru>
2024-05-03 12:15:43 +0300

    fixs test

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -12,34 +12,41 @@ $(() => {
             success: function(response) {
                 if (response.error === 0) {
 					$(".popup__content").hide();
+					$(".popup__button").hide();
 					$(".popup__answer").css('display', 'flex');
                     $('.popup__title.answer').text('Заявка на участие в Shkulev sup-завтраке отправлена.');
 					$('.popup__info.small').text('Подтверждение участия вы получите на электронную почту, которую указали в заявке.');
                 } else {
 					$(".popup__content").hide();
+					$(".popup__button").hide();
 					$(".popup__answer").css('display', 'flex');
                     $('.popup__title.answer').text('Что-то пошло не так.');
 					$('.popup__info.small').text('Попробуйте заполнить заявку снова. При повторном возникновении этой проблемы, напишите нам на почту');
-					$('.popup__title.answer').text('supbreakfast@sholding.ru.');
+					$('.popup__mail').text('supbreakfast@sholding.ru.');
 					$(".popup__btn-close.answer").click(function(){
-						$(".popup__wrap").css('display', 'none');
+						$(".popup__answer").css('display', 'none');
+						$(".popup__content").show();
+						$(".popup__button").show();
 					});
                 }
             },
             error: function() {
 				$(".popup__content").hide();
+				$(".popup__button").hide();
 				$(".popup__answer").css('display', 'flex');
 				$('.popup__title.answer').text('Что-то пошло не так.');
 				$('.popup__info.small').text('Попробуйте заполнить заявку снова. При повторном возникновении этой проблемы, напишите нам на почту');
 				$('.popup__mail').text('supbreakfast@sholding.ru.');
 				$(".popup__btn-close.answer").click(function(){
-					$(".popup__wrap").css('display', 'none');
+					$(".popup__answer").css('display', 'none');
+					$(".popup__content").show();
+					$(".popup__button").show();
 				});
             }
         });
     });
 
-	$(".popup__btn-close").click(function(){
-        $(".popup__wrap").css('display', 'none');
-    });
+	// $(".popup__btn-close").click(function(){
+    //     $(".popup__wrap").css('display', 'none');
+    // });
 });
